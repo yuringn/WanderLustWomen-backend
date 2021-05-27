f874f5953b32f5492a7246a121526781130fdda6
Yuri Nguyen <yuringuyen312@gmail.com>
2021-05-27 01:59:01 -0400

    minor change

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :country, :title, :visit_date, :review, :picture, :likes_count, :user_id
+  attributes :id, :country, :title, :visit_date, :review, :picture, :user_id, :likes
   has_one :user
   has_many :comments
   has_many :likes
